40285cc060616a9f0321bd5504fd7ac53c7b874c
Galen Warren <galen.warren@vertical1hq.com>
2018-03-25 11:36:37 -0400

    add babel-plugin-transform-builtin-classes plugin

--- dist/vue-wc-wrapper.global.js
+++ dist/vue-wc-wrapper.global.js
@@ -98,6 +98,8 @@ function getAttributes (node) {
   return res
 }
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -105,6 +107,30 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _fixBabelExtend = function (O) {
+  var gPO = O.getPrototypeOf || function (o) {
+    return o.__proto__;
+  },
+      sPO = O.setPrototypeOf || function (o, p) {
+    o.__proto__ = p;
+    return o;
+  },
+      construct = (typeof Reflect === 'undefined' ? 'undefined' : _typeof(Reflect)) === 'object' ? Reflect.construct : function (Parent, args, Class) {
+    var Constructor,
+        a = [null];
+    a.push.apply(a, args);
+    Constructor = Parent.bind.apply(Parent, a);
+    return sPO(new Constructor(), Class.prototype);
+  };
+
+  return function fixBabelExtend(Class) {
+    var Parent = gPO(Class);
+    return sPO(Class, sPO(function Super() {
+      return construct(Parent, arguments, gPO(this).constructor);
+    }, Parent));
+  };
+}(Object);
 
 function wrap(Vue, Component) {
   var isAsync = typeof Component === 'function' && !Component.cid;
@@ -176,7 +202,7 @@ function wrap(Vue, Component) {
     el._wrapper.props[camelized] = convertAttributeValue(value, key, camelizedPropsMap[camelized]);
   }
 
-  var CustomElement = function (_HTMLElement) {
+  var CustomElement = _fixBabelExtend(function (_HTMLElement) {
     _inherits(CustomElement, _HTMLElement);
 
     function CustomElement() {
@@ -276,7 +302,7 @@ function wrap(Vue, Component) {
     }]);
 
     return CustomElement;
-  }(HTMLElement);
+  }(HTMLElement));
 
   if (!isAsync) {
     initialize(Component);
