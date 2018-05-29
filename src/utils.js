55bf9af9a454182f53ba17df441ae09e4ce41b0b
Peter Soots <peter.soots@downstream.com>
2018-05-29 16:10:59 -0700

    feat: more string => prop conversions

--- src/utils.js
+++ src/utils.js
@@ -40,8 +40,9 @@ export function createCustomEvent (name, args) {
 
 const isBoolean = val => /function Boolean/.test(String(val))
 const isNumber = val => /function Number/.test(String(val))
+const isObject = val => /function Object/.test(String(val));
 
-export function convertAttributeValue (value, name, { type } = {}) {
+function convertAttributeValue (value, name, { type } = {}) {
   if (isBoolean(type)) {
     if (value === 'true' || value === 'false') {
       return value === 'true'
@@ -50,9 +51,19 @@ export function convertAttributeValue (value, name, { type } = {}) {
       return true
     }
     return value != null
+  } else if (value === '') {
+    return null
   } else if (isNumber(type)) {
-    const parsed = parseFloat(value, 10)
+    const parsed = parseFloat(value, 10);
     return isNaN(parsed) ? value : parsed
+  } else if (isObject(type) && value !== undefined) {
+    try {
+      return JSON.parse(value)
+    } catch (error) {
+      console.error("Error parsing attribute value from JSON:", value)
+      console.error(error)
+    }
+    return null
   } else {
     return value
   }
