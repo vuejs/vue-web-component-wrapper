22652a1cef630fab242ad641cf1d5d5dea520ab8
Steve Repsher <steverep@users.noreply.github.com>
2023-01-09 01:13:18 -0500

    Correct return type to CustomElementConstructor

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,6 @@ import _Vue, { Component, AsyncComponent } from 'vue'
 declare function wrap(
   Vue: typeof _Vue,
   Component: Component | AsyncComponent
-): HTMLElement
+): CustomElementConstructor
 
 export default wrap
